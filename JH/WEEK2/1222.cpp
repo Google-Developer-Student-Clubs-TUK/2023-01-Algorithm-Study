49eab1f21dd056212afced4dc1bb3b800bf73f06
jun <ggprgrkjh@naver.com>
2023-05-02 23:15:22 +0900

    test

---
+++ JH/WEEK2/1222.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int b,c,d;
+    cin >> b>>c>>d;
+
+    vector<int>burger;
+    vector<int>side;
+    vector<int>drink;
+
+    int allvalue=0;
+
+    for(int i = 0 ; i < b ; i ++){
+        int a;
+        cin >> a;
+        burger.push_back(a);
+        allvalue+=a;
+
+    }
+    for(int i = 0 ; i < c ; i ++){
+        int a;
+        cin >> a;
+        side.push_back(a);
+        allvalue+=a;
+
+
+    }
+    for(int i = 0 ; i < d ; i ++){
+        int a;
+        cin >> a;
+        drink.push_back(a);
+        allvalue+=a;
+
+    }
+
+
+    sort(burger.begin(),burger.end(),greater<>());
+    sort(side.begin(),side.end(),greater<>());
+    sort(drink.begin(),drink.end(),greater<>());
+
+    int minvalue = min(burger.size(),min(side.size(),drink.size()));
+    int maxvalue = max(burger.size(),max(side.size(),drink.size()));
+    int sum =0;
+
+    for(int i = 0 ; i < minvalue; i++){
+        int discount = (burger[i]+side[i]+drink[i]) * 0.9;
+        sum += discount;
+        //cout << discount <<"\n";
+    }
+
+    for(int i = minvalue; i < )
+
+    //cout << "\n";
+
+    cout << allvalue <<"\n";
+    cout << sum <<"\n";
+
+
+
+
+
+
+}
